55db962e12ffba001075c8c9fe02d1bdda1b9a43
tschenhau <72181130+tschenhau@users.noreply.github.com>
2023-01-12 00:31:01 -0500

    #16 Correct DB error

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -14,8 +14,10 @@ func TestCreateAccount(t *testing.T) {
 }
 
 func createRandomAccount(t *testing.T) Account {
+	user := createRandomUser(t)
+
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
